e996b70e93d58070420624b32eedbe14bb338055
Kaio Silveira <silveira.kaio@icloud.com>
2024-10-19 15:08:58 +0100

    [base-ex]: introduce envisioned signature for `withinRange`

--- src/example/heating-plan/index.js
+++ src/example/heating-plan/index.js
@@ -6,4 +6,8 @@ export class HeatingPlan {
   withinRange(bottom, top) {
     return bottom >= this._temperatureRange.low && top <= this._temperatureRange.high;
   }
+
+  xxNEWWithinRange(aNumberRange) {
+    return this.withinRange(aNumberRange.low, aNumberRange.high);
+  }
 }
