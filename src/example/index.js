43afcc48a036d1d607b5a1685acf723795e84604
Kaio Silveira <silveira.kaio@icloud.com>
2024-10-19 15:15:39 +0100

    [base-ex]: remove unused temp vars

--- src/example/index.js
+++ src/example/index.js
@@ -1,8 +1,6 @@
 export function alertIfPlanIsNotWithinRange(aRoom, aPlan) {
   const alerts = [];
 
-  const low = aRoom.daysTempRange.low;
-  const high = aRoom.daysTempRange.high;
   if (!aPlan.xxNEWWithinRange(aRoom.daysTempRange)) {
     alerts.push('room temperature went outside range');
   }
