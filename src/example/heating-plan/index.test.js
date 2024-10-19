463fa1d0120d199ec3f578f65aade6402cb9ee3e
Kaio Silveira <silveira.kaio@icloud.com>
2024-10-19 15:19:07 +0100

    [base-ex]: update tests to use only `xxNEWWithinRange`

--- src/example/heating-plan/index.test.js
+++ src/example/heating-plan/index.test.js
@@ -5,12 +5,12 @@ describe('HeatingPlan', () => {
     const aPlan = new HeatingPlan({ low: 10, high: 20 });
 
     it('should return true if the plan is within range', () => {
-      const result = aPlan.withinRange(15, 18);
+      const result = aPlan.xxNEWWithinRange({ low: 15, high: 18 });
       expect(result).toBe(true);
     });
 
     it('should return false if the plan is not within range', () => {
-      const result = aPlan.withinRange(5, 25);
+      const result = aPlan.xxNEWWithinRange({ low: 5, high: 25 });
       expect(result).toBe(false);
     });
   });
